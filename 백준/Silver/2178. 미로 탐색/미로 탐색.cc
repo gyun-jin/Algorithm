0d47e4ee3d699cad149173fc9911e00cef0d4bc7
qkrbswls <138318248+qkrbswls@users.noreply.github.com>
2023-07-17 15:56:06 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2072 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,52 +1,51 @@
-// 미로 탐색
 #include<iostream>
 #include<queue>
 using namespace std;
 
-int a[100][100];
-bool visit[100][100];
-int by[4] = { 1,0,-1,0 };
-int bx[4] = { 0,1,0,-1 };
 int n, m;
+int bx[4] = { 1,0,-1,0 };
+int by[4] = { 0,1,0,-1 };
+bool visit[100][100];
+int v[100][100];
 
-void bfs(int r, int c) {
+void bfs(int x, int y) {
 	queue<pair<int, int>> q;
-	q.push({ r,c });
-	visit[r][c] = 1;
+	q.push({ x,y });
+	visit[x][y] = 1;
 
-	while (!q.empty()) {
-		int r = q.front().first;
-		int c = q.front().second;
+	while(!q.empty()) {
+		x = q.front().first;
+		y = q.front().second;
 		q.pop();
 
-		for (int i = 0; i < 4; i++) {
-			int rr = r + by[i];
-			int cc = c + bx[i];
+		for (int j = 0; j < 4; j++) {
+			int xx = x + bx[j];
+			int yy = y + by[j];
 
-			if (rr < 0 || cc < 0 || rr >= n || cc >= m) continue;
-			if (a[rr][cc] == 0 || visit[rr][cc] == 1) continue;
+			if (xx < 0 || xx >= n || yy < 0 || yy >= m) continue;
+			if (v[xx][yy] == 0 || visit[xx][yy]) continue;
 
-			a[rr][cc] = a[r][c] + 1;
-			visit[rr][cc] = 1;
-			q.push({ rr,cc });
+			q.push({ xx,yy });
+			visit[xx][yy] = 1;
+			v[xx][yy] = v[x][y] + 1;
 		}
 	}
+
 }
 
 int main() {
-	cin.tie(NULL);
-	ios_base::sync_with_stdio(false);
-
 	cin >> n >> m;
+	cin.ignore();
 
-	string String;
 	for (int i = 0; i < n; i++) {
-		cin>>String;
+		string s;
+		cin >> s;
 		for (int j = 0; j < m; j++) {
-			a[i][j] = String[j] - '0';
+			v[i][j] = s[j] - '0';
 		}
 	}
-	bfs(0, 0);
-	cout << a[n - 1][m - 1] << endl;
-}
 
+	bfs(0, 0);
+
+	cout << v[n - 1][m-1];
+}
